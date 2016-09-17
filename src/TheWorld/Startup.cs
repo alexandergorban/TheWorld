a84a645c61ed30b6e63a4e6b136e77f46fcd9f6e
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-09-17 17:23:10 +0300

    Initial commit (HTML, CSS, JS)

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -21,17 +21,20 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            app.UseDefaultFiles();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStaticFiles();
+            //loggerFactory.AddConsole();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello 2 World!");
+            //});
         }
     }
 }
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿/* site.js */
+
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "ASP.NET Core";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style = "background: #888";
+    }
+    main.onmouseleave = function() {
+        main.style = "";
+    }
+})();
+
+
