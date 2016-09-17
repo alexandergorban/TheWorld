c5201219394c0f4d69f187f15844b51c9bb784d2
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-09-17 20:23:14 +0300

    base jQuery

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,16 +1,48 @@
 ﻿/* site.js */
 
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "ASP.NET Core";
+    $('#username').text("ASP.NET Core");
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function() {
-        main.style = "background: #888";
-    }
-    main.onmouseleave = function() {
-        main.style = "";
-    }
+    // -- or --
+    //var ele = $("#username");
+    //ele.text("ASP.NET Core");
+
+    $('#main').hover(function () {
+        $(this).on('mouseenter', function () {
+            this.style = "background-color: #888;";
+        });
+    }, function () {
+        $(this).on('mouseleave', function () {
+            this.style = "";
+        });
+    });
+
+    // -- or with JS --
+    //var main = document.getElementById("main");
+    //main.onmouseenter = function() {
+    //    main.style = "background-color: #888;";
+    //};
+    //main.onmouseleave = function() {
+    //    main.style = "";
+    //};
+
+    $('ul.menu li a')
+        .hover(function () {
+            $(this)
+                .on('click',
+                    function () {
+                        alert($(this).text());
+                    });
+        });
+
+    // -- or --
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click",
+    //    function () {
+    //        var me = $(this);
+    //        alert(me.text());
+    //    });
+
 })();
 
 
