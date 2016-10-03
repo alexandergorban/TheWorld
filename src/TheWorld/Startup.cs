bb28056179cfaabc7489349b6b45e7b14f644336
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-03 16:29:33 +0300

    EF Core. Seeding the Database.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -47,11 +47,13 @@ namespace TheWorld
 
             services.AddDbContext<WorldContext>();
 
+            services.AddTransient<WorldContextSeedData>();
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WorldContextSeedData seeder)
         {
             if (env.IsEnvironment("Development"))
             {
@@ -66,6 +68,8 @@ namespace TheWorld
                     defaults: new { controller = "App", action = "Index" }
                 );
             });
+
+            seeder.EnsureSeedData().Wait();
         }
     }
 }
