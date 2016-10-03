b46fcc037308b700e6a9863904b16b00687175f3
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-04 01:13:34 +0300

    EF Core. Logging.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -51,15 +51,25 @@ namespace TheWorld
 
             services.AddTransient<WorldContextSeedData>();
 
+            services.AddLogging();
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WorldContextSeedData seeder)
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            WorldContextSeedData seeder,
+            ILoggerFactory factory)
         {
             if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                factory.AddDebug(LogLevel.Error);
             }
             app.UseStaticFiles();
             app.UseMvc(config =>
@@ -75,3 +85,4 @@ namespace TheWorld
         }
     }
 }
+
