6082517c7c30863785dc45847ddbe3b9ccdf4d72
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-06 11:21:05 +0300

    Validation and View Models

--- src/TheWorld/Controllers/Api/TripsController.cs
+++ src/TheWorld/Controllers/Api/TripsController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Api
 {
@@ -25,9 +26,14 @@ namespace TheWorld.Controllers.Api
         }
 
         [HttpPost("")]
-        public IActionResult Post([FromBody]Trip theTrip)
+        public IActionResult Post([FromBody]TripViewModel theTrip)
         {
-            return Ok(true);
+            if (ModelState.IsValid)
+            {
+                return Created($"api/trips/{theTrip.Name}", theTrip);
+            }
+
+            return BadRequest("Bad data");
         }
     }
 }
