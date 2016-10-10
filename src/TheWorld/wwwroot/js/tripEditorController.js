27b6bd971e6dcc115b52d1d61649de59d8445eaa
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-10 17:29:01 +0300

    AngularJS Routing. Calling the API (stops)

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -1,16 +1,29 @@
 ﻿// tripEditorController.js
-(function() {
+(function () {
     "use strict";
 
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         vm.tripName = $routeParams.tripName;
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function (response) {
+                // success
+                angular.copy(response.data, vm.stops);
+            },
+                // failure
+                function (err) {
+                    vm.errorMessage = "Failed to load stops";
+                })
+            .finally(function () {
+                vm.isBusy = false;
+            });
     }
 })();
