1c63e96e5afbeccdff9c336faf77a5d3bd8ce214
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-10 17:21:20 +0300

    AngularJS Routing. Implementing the Editor

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -5,9 +5,12 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController() {
+    function tripEditorController($routeParams) {
         var vm = this;
 
-        vm.name = "Alex";
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
     }
 })();
