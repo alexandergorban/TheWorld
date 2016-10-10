a804e06fe9377fefb500d697f9d360a81bc6b5a0
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-10 13:21:35 +0300

    AngularJS. Handling Forms

--- src/TheWorld/wwwroot/js/tripsController.js
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -19,6 +19,12 @@
             name: "World Trip",
             created: new Date
         }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function() {
+            alert(vm.newTrip.name);
+        };
     }
 
 })();
